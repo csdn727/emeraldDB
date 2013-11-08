6a8bb1775f144622e169a50ea23874d593da4e8c
csdn727 <csdn727@gmail.com>
2013-11-08 16:57:42 +0800

    snapshot

--- src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "ossPrimitiveFileOp.hpp"
-
-int main(void)
-{
-   ossPrimitiveFileOp _hello;
-   _hello.Open ("./_hello.txt", (((unsigned int)1) << 4)) ;
-   _hello.Write( "hello!!~", sizeof(("hello!!~")+1) ) ;
-}
-
